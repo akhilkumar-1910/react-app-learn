b9b21ebdb774bfa7ad90289258a0613eaa30201f
Akhil Kumar Tatipamula <akhilkumar1910@gmail.com>
2022-04-24 20:40:23 +0530

    added style to navbar component

--- quotesland-by-akhil/src/components/Navbarcomponent.jsx
+++ quotesland-by-akhil/src/components/Navbarcomponent.jsx
@@ -1,18 +1,23 @@
-import {Navbar, Nav, Container} from 'react-bootstrap';
+import {Navbar, Nav, Container, Row} from 'react-bootstrap';
 
 const Navbarcomponent = () => {
     return (
         <Navbar bg="primary" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">Quotesland</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#more_quotes">More Quotes</Nav.Link>
-                    <Nav.Link href="#about">About</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
+            <Container fluid>
+                <Row md={2}>
+                    <Navbar.Brand href="#home" style={{color: "white"}}>Quotesland</Navbar.Brand>
+                </Row>
+                <Row md={6}></Row>
+                <Row md={4}>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home" style={{color: "white"}}>Home</Nav.Link>
+                        <Nav.Link href="#more_quotes" style={{color: "white"}}>More Quotes</Nav.Link>
+                        <Nav.Link href="#about" style={{color: "white"}}>About</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Row>
             </Container>
         </Navbar>
     )
